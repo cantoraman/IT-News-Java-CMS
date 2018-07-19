90165953366ecd99bfa8819c45c8ec885c3d30fe
Can Toraman <cantoraman@gmail.com>
2018-07-19 13:07:35 +0100

    works on article db relations

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -1,14 +1,19 @@
 package models;
 
+
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name="articles")
 public class Article {
 
     private int id;
-private String title;
-private String body;
-private String imageURL;
-private Journalist journalist;
+    private String title;
+    private String body;
+    private String imageURL;
+    private Journalist journalist;
 
     public Article(String title, String body, String imageURL, Journalist journalist) {
         this.title = title;
@@ -17,49 +22,50 @@ private Journalist journalist;
         this.journalist = journalist;
     }
 
-  public Article(){}
+    public Article(){}
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
 
+    @Column(name="title")
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public Journalist getJournalist() {
-        return journalist;
-    }
-
-    public void setJournalist(Journalist journalist) {
-        this.journalist = journalist;
-    }
-
+    @Column(name="body")
     public String getBody() {
         return body;
     }
-
     public void setBody(String body) {
         this.body = body;
     }
 
-
+    @Column(name="image")
     public String getImageURL() {
         return imageURL;
     }
-
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
 
+    @Column(name="journalist")
+    public Journalist getJournalist() {
+        return journalist;
+    }
+    public void setJournalist(Journalist journalist) {
+        this.journalist = journalist;
+    }
 
 
 }
