c234e9161157a3ff863e2747e478d13cb8398ecc
Can Toraman <cantoraman@gmail.com>
2018-07-19 14:19:04 +0100

    adds main controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,38 @@
 package controller;
 
+import db.Seeds;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class Controller {
+
+    public static void main(String[] args) {
+        Seeds.seedData();
+
+
+        ArticleController articleController = new ArticleController();
+        JournalistController journalistController = new JournalistController();
+
+
+        get("/", (req, res) -> {
+
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
+    }
+
 }
+
+
+
+
+
+
---
+++ src/main/java/db/Seeds.java
@@ -0,0 +1,41 @@
+package db;
+
+import models.Article;
+import models.Journalist;
+import models.Article;
+
+
+public class Seeds {
+
+
+    public static void seedData() {
+        DBHelper.deleteAll(Article.class);
+        DBHelper.deleteAll(Journalist.class);
+//
+//        Department department1 = new Department("HR");
+//        DBHelper.save(department1);
+//        Department department2 = new Department("IT");
+//        DBHelper.save(department2);
+//        Manager manager = new Manager("Peter", "Griffin", 40000,department1, 100000 );
+//        DBHelper.save(manager);
+//        Engineer engineer1 = new Engineer("Lois", "Griffin", 29000, department1);
+//        DBHelper.save(engineer1);
+//        Engineer engineer2 = new Engineer("Stewie", "Griffin", 27000, department1);
+//        DBHelper.save(engineer2);
+
+
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
