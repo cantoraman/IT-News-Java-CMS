a1746b40b6cff6c4c216f76b05d41db8dade44ac
jesus2102 <jesusperezmarin@gmail.com>
2018-07-19 11:27:19 +0100

    Journalist and Category SET

--- src/main/java/models/Journalist.java
+++ src/main/java/models/Journalist.java
@@ -1,4 +1,60 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "journalist")
 public class Journalist {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private List<Article> articlest;
+
+    public Journalist() {
+    }
+
+    public Journalist(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @OneToMany(mappedBy = "journalist", fetch = FetchType.LAZY)
+    public List<Article> getArticlest() {
+        return articlest;
+    }
+
+    public void setArticlest(List<Article> articlest) {
+        this.articlest = articlest;
+    }
 }
