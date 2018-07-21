36ae18ce31b7eadc2af53c7894290658ea11583d
jesus2102 <jesusperezmarin@gmail.com>
2018-07-21 09:55:40 +0100

    header v1 done

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,11 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 
-    public class Controller {
+public class Controller {
 
         public static void main(String[] args) {
+            VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+            staticFileLocation("/public");
             Seeds.seedData();
 
 
@@ -25,7 +28,7 @@ import static spark.Spark.get;
                 Map<String, Object> model = new HashMap();
                 model.put("template", "templates/index.vtl");
                 return new ModelAndView(model, "templates/layout.vtl");
-            }, new VelocityTemplateEngine());
+            }, velocityTemplateEngine);
 
 
         }
