25bf8b68b5b43161839f8765254393e61b022018
Can Toraman <cantoraman@gmail.com>
2018-07-23 14:33:36 +0100

    removes system pause on seeding, replaces with date adjustment, adds another seed

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -4,6 +4,7 @@ import models.Article;
 import models.Category;
 import models.Journalist;
 import models.Article;
+import org.apache.commons.lang.time.DateUtils;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,27 +25,31 @@ public class Seeds {
         Article article1 = new Article("Java developers on Demand", "IT Companies demand Developers", Category.JAVA, journalist1);
         article1.setLikes(5);
         article1.setDislikes(2);
+        article1.setDate(DateUtils.addDays(article1.getDate(),-5));
         DBHelper.save(article1);
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         Article article2 = new Article("Java developers close to kill each others", "IT Companies demand Developers", Category.JAVA, journalist1);
         article2.setLikes(50);
         article2.setDislikes(20);
+        article2.setDate(DateUtils.addDays(article2.getDate(),-2));
         DBHelper.save(article2);
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
 
         Article article3 = new Article("Elon Musk makes space-pancakes!", "He makes awesome pancakes and hipsters lose their minds.", Category.TECH, journalist2);
         article3.setLikes(1);
         article3.setDislikes(90);
+        article3.setDate(DateUtils.addDays(article3.getDate(),-1));
+
         DBHelper.save(article3);
-//
+
+        Article article4 = new Article("Tourist lands on the Moon!", "And claims compensation for lack of oxygen", Category.HISTORY, journalist2);
+        article4.setLikes(25);
+        article4.setDislikes(3);
+
+        DBHelper.save(article4);
+
+
+
 //        Department department1 = new Department("HR");
 //        DBHelper.save(department1);
 //        Department department2 = new Department("IT");
